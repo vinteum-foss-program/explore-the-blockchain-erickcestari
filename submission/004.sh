9668cf191d0a2a009491d0388238e73c836980fb
Erick Cestari <erickcestari03@gmail.com>
2024-11-20 14:17:44 -0300

    submit 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,6 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+
+xpub=xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2
+
+bitcoin-cli deriveaddresses "tr($xpub/100)#5p2mg7zx" | jq -r .[]
