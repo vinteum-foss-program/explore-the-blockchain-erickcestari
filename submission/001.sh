114e02238c6a818dfe34bc72212edd3e5a2cd0db
Erick Cestari <erickcestari03@gmail.com>
2024-11-20 10:53:50 -0300

    fix submit 001.sh

--- submission/001.sh
+++ submission/001.sh
@@ -1,8 +1,3 @@
 # What is the hash of block 654,321?
 
-```
-❯ bitcoin-cli getblockhash 654321
-```
-
-000000000000000000058452bbe379ad4364fe8fda68c45e299979b492858095
-
+bitcoin-cli getblockhash 654321
