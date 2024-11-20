59adebeda228b04dc9e7d5079c2cfbc124f82f22
Erick Cestari <erickcestari03@gmail.com>
2024-11-20 15:40:16 -0300

    fix submit 006.sh

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,17 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+
+blockhash_coinbase=$(bitcoin-cli getblockhash 256128)
+coinbase=$(bitcoin-cli getblock "$blockhash_coinbase" true | jq -r '.tx[0]')
+
+blockhash=$(bitcoin-cli getblockhash 257343)
+txids=$(bitcoin-cli getblock "$blockhash" true | jq -r '.tx[]')
+
+for txid in $txids; do
+  txinputs=$(bitcoin-cli getrawtransaction "$txid" true | jq -r '.vin[].txid')
+  for txinput in $txinputs; do
+    if [[ "$txinput" == "$coinbase" ]]; then
+      echo $txid
+      break 2
+    fi
+  done
+done
